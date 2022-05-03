b27793a16644d61dd6d1709e8b44b99dcc9388c1
eb <92720715+ebbebe@users.noreply.github.com>
2022-05-03 22:56:29 +0900

    Add files via upload

--- ne.py
+++ ne.py
@@ -1,20 +1,44 @@
-import sys 
-from PyQt5.QtWidgets import *
-from PyQt5.QAxContainer import *
+#참고용
+
+# 로그인
+# kiwoom = Kiwoom()
+# kiwoom.CommConnect(block=True)
+# print("로그인 완료")
+# post_message(myToken, "#noti", "로그인 완료")
+
+# # 조건식을 PC로 다운로드
+# kiwoom.GetConditionLoad()
+
+# # 전체 조건식 리스트 얻기
+# conditions = kiwoom.GetConditionNameList()
+
+# # 검색할 조건식 설정
+# myindex = 69
+# condition_index = conditions[myindex][0]
+# condition_name = conditions[myindex][1]
+    
+# #주식계좌
+# accounts = kiwoom.GetLoginInfo("ACCNO")
+# stock_account = accounts[0]
+
+############################################
+# 배열 문자열 배열로 변환
+# for i in range(len(conditions)):
+#     conditions[i] = str(conditions[i])
+
+# str = ',\n'.join(conditions)
+# print(str)
+############################################
+
+# 문자열 slack에 메시지 보내기
+# post_message(myToken,"#noti",str)
 
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        self.ocx.dynamicCall("CommConnect()")
-        self.ocx.OnEventConnect.connect(self.OnEventConnect)
-
-    def OnEventConnect(self, err_code):
-        print(err_code)
 
 
-app = QApplication(sys.argv)
-window = MyWindow()
-window.show()
-app.exec_()
+
+# buy = kiwoom.SendOrder("시장가매수 테스트", "0331", stock_account, 1, "005930", 1, 0, "03", "")
+# print("매수시도:" , buy)
+
+# sell = kiwoom.SendOrder("시장가매도 테스트", "0101", stock_account, 2, "005930", 10, 0, "03", "")
+# print("매도시도:" , sell)
